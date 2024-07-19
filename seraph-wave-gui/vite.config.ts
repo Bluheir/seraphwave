3627c5754b6eed7fe3636fa4822f0902e4f1b6fd
Bluheir <slimcognitodudesvids@gmail.com>
2024-07-19 12:46:36 +1000

    Add HTTPS for vite dev server.

--- seraph-wave-gui/vite.config.ts
+++ seraph-wave-gui/vite.config.ts
@@ -1,6 +1,33 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite"
+import { sveltekit } from "@sveltejs/kit/vite"
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
+import wasm from "vite-plugin-wasm"
+import topLevelAwait from "vite-plugin-top-level-await"
+import basicSsl from "@vitejs/plugin-basic-ssl"
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+	plugins: [
+		sveltekit(),
+		wasm(),
+		topLevelAwait(),
+		basicSsl({
+			name: "seraphwave",
+			domains: ["seraphwave"],
+		}),
+	],
+	server: {
+		proxy: {},
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			define: {
+				global: "globalThis",
+			},
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					buffer: true,
+				}),
+			],
+		},
+	},
+})
