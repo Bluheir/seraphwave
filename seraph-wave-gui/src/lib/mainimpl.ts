c588be10be84236a59b486f5615e766b74d4967d
Bluheir <slimcognitodudesvids@gmail.com>
2024-07-29 20:03:42 +1000

    Add notifications in GUI for potential errors.

--- seraph-wave-gui/src/lib/mainimpl.ts
+++ seraph-wave-gui/src/lib/mainimpl.ts
@@ -1,5 +1,5 @@
 import type {
-    AudioEvent,
+	AudioEvent,
 	CreateCode,
 	FullApi,
 	GetMetaInfo,
@@ -7,8 +7,8 @@ import type {
 	MetaInfo,
 	ProtocolError,
 	SessionCodeReceive,
-    VoiceClient,
-    VoiceClientMgr,
+	VoiceClient,
+	VoiceClientMgr,
 } from "./api"
 
 export class MainImpl implements CreateCode, GetMetaInfo, FullApi, VoiceClientMgr {
@@ -22,10 +22,24 @@ export class MainImpl implements CreateCode, GetMetaInfo, FullApi, VoiceClientMg
 	async getMeta(): Promise<MetaInfo & any> {
 		return await (await fetch("/meta")).json()
 	}
-    async createClient(info: MetaInfo & any, initialState: InitialConnState): Promise<VoiceClient> {
-        const ws = new WebSocket(info.webSocketUrl)
-        return new MainVoiceClient(ws, initialState)
-    }
+	createClient(info: MetaInfo & any, initialState: InitialConnState): Promise<VoiceClient> {
+		const ws = new WebSocket(info.webSocketUrl)
+
+        async function voiceClient() {
+            const vc = new MainVoiceClient(ws, initialState)
+            await vc.start()
+            return vc
+        }
+		
+        return new Promise<VoiceClient>((resolve, reject) => {
+            ws.onopen = () => {
+                resolve(voiceClient())
+            }
+            ws.onerror = e => {
+                reject("cannot connect")
+            }
+        })
+	}
 }
 
 type ConnectionState =
@@ -37,6 +51,9 @@ type ConnectionState =
 			type: "offline"
 	  }
 	| {
+			type: "awaitFirstUpdate"
+	  }
+	| {
 			type: "online"
 	  }
 type PlayerUpdate = {
@@ -45,39 +62,39 @@ type PlayerUpdate = {
 }
 
 function parseAudioArr(buffer: ArrayBuffer): AudioEvent {
-    const view = new DataView(buffer)
+	const view = new DataView(buffer)
 
-    const type = view.getInt8(0)
+	const type = view.getInt8(0)
 
-    // audio packet
-    if(type === 0) {
-        return {
-            type: "audioPacket",
-            uuid: (view.getBigUint64(1) << BigInt(64)) | view.getBigUint64(9),
-            pos: {
-                x: view.getFloat64(17),
-                y: view.getFloat64(25),
-                z: view.getFloat64(33)
-            },
-            dir: {
-                x: view.getFloat64(41),
-                y: view.getFloat64(49),
-                z: view.getFloat64(57)
-            },
-            buf: view.buffer.slice(65, view.buffer.byteLength)
-        }
-    }
-    // rotation update
-    else {
-        return {
-            type: "rotationUpdate",
-            rotation: {
-                x: view.getFloat64(1),
-                y: view.getFloat64(9),
-                z: view.getFloat64(17),
-            }
-        }
-    }
+	// audio packet
+	if (type === 0) {
+		return {
+			type: "audioPacket",
+			uuid: (view.getBigUint64(1) << BigInt(64)) | view.getBigUint64(9),
+			pos: {
+				x: view.getFloat64(17),
+				y: view.getFloat64(25),
+				z: view.getFloat64(33),
+			},
+			dir: {
+				x: view.getFloat64(41),
+				y: view.getFloat64(49),
+				z: view.getFloat64(57),
+			},
+			buf: view.buffer.slice(65, view.buffer.byteLength),
+		}
+	}
+	// rotation update
+	else {
+		return {
+			type: "rotationUpdate",
+			rotation: {
+				x: view.getFloat64(1),
+				y: view.getFloat64(9),
+				z: view.getFloat64(17),
+			},
+		}
+	}
 }
 
 export class MainVoiceClient implements VoiceClient {
@@ -85,27 +102,31 @@ export class MainVoiceClient implements VoiceClient {
 	private connState: ConnectionState
 
 	// events
-	private onUserDataI: ((data: SessionCodeReceive) => Promise<void>) | ((data: SessionCodeReceive) => void)
-    private onCloseI: (() => Promise<void>) | (() => void)
-    private onAudioI: ((packet: AudioEvent) => Promise<void>) | ((packet: AudioEvent) => void)
-    private onConnectI: (() => void) | (() => Promise<void>)
-    private onErrorI: ((err: ProtocolError) => void) | ((err: ProtocolError) => Promise<void>)
-    
-    onSessionCode(ev: ((data: SessionCodeReceive) => Promise<void>) | ((data: SessionCodeReceive) => void)) {
+	private onUserDataI:
+		| ((data: SessionCodeReceive) => Promise<void>)
+		| ((data: SessionCodeReceive) => void)
+	private onCloseI: (() => Promise<void>) | (() => void)
+	private onAudioI: ((packet: AudioEvent) => Promise<void>) | ((packet: AudioEvent) => void)
+	private onConnectI: (() => void) | (() => Promise<void>)
+	private onErrorI: ((err: ProtocolError) => void) | ((err: ProtocolError) => Promise<void>)
+
+	onSessionCode(
+		ev: ((data: SessionCodeReceive) => Promise<void>) | ((data: SessionCodeReceive) => void),
+	) {
 		this.onUserDataI = ev
 	}
-    onClose(ev: (() => void) | (() => Promise<void>)): void {
-        this.onCloseI = ev
-    }
-    onAudio(ev: ((packet: AudioEvent) => Promise<void>) | ((packet: AudioEvent) => void)): void {
-        this.onAudioI = ev
-    }
-    onConnect(ev: (() => void) | (() => Promise<void>)): void {
-        this.onConnectI = ev
-    }
-    onError(ev: ((err: ProtocolError) => void) | ((err: ProtocolError) => Promise<void>)): void {
-        this.onErrorI = ev
-    }
+	onClose(ev: (() => void) | (() => Promise<void>)): void {
+		this.onCloseI = ev
+	}
+	onAudio(ev: ((packet: AudioEvent) => Promise<void>) | ((packet: AudioEvent) => void)): void {
+		this.onAudioI = ev
+	}
+	onConnect(ev: (() => void) | (() => Promise<void>)): void {
+		this.onConnectI = ev
+	}
+	onError(ev: ((err: ProtocolError) => void) | ((err: ProtocolError) => Promise<void>)): void {
+		this.onErrorI = ev
+	}
 
 	constructor(client: WebSocket, connState: InitialConnState) {
 		this.client = client
@@ -113,76 +134,86 @@ export class MainVoiceClient implements VoiceClient {
 		this.connState = connState
 
 		this.onUserDataI = () => {}
-        this.onCloseI = () => {}
-        this.onAudioI = () => {}
-        this.onConnectI = () => {}
-        this.onErrorI = () => {}
-        
-        client.onopen = async _ => {
-            await this.onConnectI()
-            switch(this.connState.type) {
-                case "full":
-                    this.client.send(JSON.stringify({
-                        protocolV: 0,
-                        type: "full",
-                        code: this.connState.code,
-                        uuid: this.connState.uuid
-                    }))
-                    this.connState = { type: "offline" }
-                    break
-
-                case "temp":
-                    this.client.send(JSON.stringify({
-                        protocolV: 0,
-                        ...this.connState
-                    }))
-                    this.connState = { type: "sessionCodeAwait" }
-                    break
-            }
-        }
+		this.onCloseI = () => {}
+		this.onAudioI = () => {}
+		this.onConnectI = () => {}
+		this.onErrorI = () => {}
 
 		client.onmessage = async (msg) => {
 			switch (this.connState.type) {
 				case "sessionCodeAwait":
-                    const sessionInfo = JSON.parse(msg.data)
-                    if(sessionInfo.errorCode) {
-                        await this.onErrorI(sessionInfo)
-                    } else {
-                        await this.onUserDataI(sessionInfo)
-                        this.connState = { type: "offline" }
-                    }
+					const sessionInfo = JSON.parse(msg.data)
+					if (sessionInfo.errorCode) {
+						await this.onErrorI(sessionInfo)
+					} else {
+						await this.onUserDataI(sessionInfo)
+						this.connState = { type: "awaitFirstUpdate" }
+					}
 					break
-
+				case "awaitFirstUpdate":
+					const firstUpdate = JSON.parse(msg.data)
+					if (firstUpdate.errorCode) {
+						await this.onErrorI(firstUpdate)
+					} else {
+						this.connState = { type: firstUpdate.value }
+						await this.onConnectI()
+					}
+					break
 				case "offline":
 					const update = JSON.parse(msg.data)
-                    if(update.errorCode){
-                        await this.onErrorI(update)
-                    } else {
-                        this.connState = { type: update.value }
-                    }
+					if (update.errorCode) {
+						await this.onErrorI(update)
+					} else {
+						this.connState = { type: update.value }
+					}
 					break
 
 				case "online":
 					if (typeof msg.data === "string") {
 						const update = JSON.parse(msg.data)
-                        if(update.errorCode) {
-                            await this.onErrorI(update)
-                        } else {
-                            this.connState = { type: update.value }
-                        }
+						if (update.errorCode) {
+							await this.onErrorI(update)
+						} else {
+							this.connState = { type: update.value }
+						}
 					} else {
-                        const data: ArrayBuffer = msg.data
-                        await this.onAudioI(parseAudioArr(data))                        
-                    }
+						const data: ArrayBuffer = msg.data
+						await this.onAudioI(parseAudioArr(data))
+					}
 					break
 			}
 		}
-        client.onclose = async _ => await this.onCloseI()
+		client.onclose = async (_) => await this.onCloseI()
+	}
+	async start() {
+		switch (this.connState.type) {
+			case "full":
+				this.client.send(
+					JSON.stringify({
+						protocolV: 0,
+						type: "full",
+						code: this.connState.code,
+						uuid: this.connState.uuid,
+					}),
+				)
+				this.connState = { type: "awaitFirstUpdate" }
+				break
+
+			case "temp":
+				this.client.send(
+					JSON.stringify({
+						protocolV: 0,
+						...this.connState,
+					}),
+				)
+				this.connState = { type: "sessionCodeAwait" }
+				break
+		}
 	}
 
-    async send(blob: Blob | ArrayBufferLike) {
-        if (this.connState.type == "online") {
-            this.client.send(blob)
-        }
-    }
+	async send(blob: Blob | ArrayBufferLike) {
+		if (this.connState.type == "online") {
+			this.client.send(blob)
+		}
+	}
 }
--- seraph-wave-gui/src/routes/account/+page.svelte
+++ seraph-wave-gui/src/routes/account/+page.svelte
@@ -5,7 +5,7 @@
 	import { apiImpl, AudioManager, type VoiceClient, clientInstance, audioInstance } from "$lib/api"
 
 	import { onMount } from "svelte"
-	import { goto } from "$app/navigation"
+	import { beforeNavigate, goto } from "$app/navigation"
 
 	export let data: PageData & { accountInfo: AccountInfo }
 	let inVolume: number = 100
@@ -15,6 +15,15 @@
 		return (Math.exp(gain) - 1) / (Math.E - 1)
 	}
 
+	beforeNavigate(async _ => {
+		if($audioInstance) {
+			await $audioInstance.stopAudio()
+		}
+		$clientInstance = undefined
+	})
+
+	let errorState: "ok" | "cannotConnect" | "codeConsumed" | "disconnected" = "ok"
+
 	$: if($audioInstance) { $audioInstance.setInGain(linearizeGain(inVolume / 100))}
 	$: if($audioInstance) { $audioInstance.setOutGain(linearizeGain(outVolume / 100)) }
 
@@ -22,16 +31,27 @@
 
 	async function startSessionCode(): Promise<void> {
 		const { uuid, code } = data.accountInfo
-		const client = await apiImpl.createClient(data.metaInfo, { type: "full", uuid, code })
+		let client: VoiceClient
+
+		try {
+			client = await apiImpl.createClient(data.metaInfo, { type: "full", uuid, code })
+		} catch(e) {
+			errorState = "cannotConnect"
+			return
+		}
 
 		client.onConnect(async () => {
+			errorState = "ok"
 			$clientInstance = client
 			await startConn(client)
 		})
 		client.onError(async e => {
-			console.log(e)
+			console.error(e)
 
 			switch(e.errorCode) {
+				case 1:
+					errorState = "codeConsumed"
+					break
 				// an invalid session code implies that the session code has expired or was replaced by another
 				case 2:
 					// delete the expired session code
@@ -44,6 +64,9 @@
 			}
 		})
 		client.onClose(async () => {
+			if(errorState === "ok") {
+				errorState = "disconnected"
+			}
 			if($audioInstance) {
 				await $audioInstance.stopAudio()
 			}
@@ -80,7 +103,7 @@
 		/></div>
 		<div class="flex justify-center items-center my-4 md:my-0"><h1 class="text-3xl">{data.accountInfo.username}</h1></div>
 		<div class="prose max-w-full flex-grow">
-			{#if inVc}
+			{#if inVc && errorState === "ok"}
 				<h2 class="mt-0">Microphone volume</h2>
 				<input
 					type="range"
@@ -97,20 +120,32 @@
 					bind:value={outVolume}
 					class="range range-success w-[min(25em,100%)] range-xs"
 				/>
-			{:else}
+			{:else if errorState === "ok"}
 				<h2>Join proximity chat</h2>
 				<p class="mb-4 md:mb-0">To be able to speak in proximity chat, you need to join the proximity chat server.</p>
+			{:else if errorState === "cannotConnect"}
+				<h2>Cannot connect</h2>
+				<p class="mb-4 md:mb-0">Cannot connect to proximity chat server. This may be due to the Minecraft server being offline.</p>
+			{:else if errorState === "codeConsumed"}
+				<h2>Another client is connected on this account</h2>
+				<p class="mb-4 md:mb-0">Another client is connected to the proximity chat server using this account. Please close the tab that is connected before reconnecting.</p>
+			{:else if errorState === "disconnected"}
+				<h2>Disconnected</h2>
+				<p class="mb-4 md:mb-0">You have been disconnected from the proximity chat server. This may be due to the Minecraft server turning off.</p>
 			{/if}
 		</div>
-		{#if !inVc}
+		{#if inVc}
+		<div/>
+		{:else if errorState === "ok"}
 			<button
 				class="btn btn-accent"
-				on:click={async () =>
-					await startSessionCode()}
-				>Join proximity chat</button
-			>
-		{:else}
-		<div/>
+				on:click={async () => await startSessionCode()}
+			>Join proximity chat</button>
+		{:else if errorState === "cannotConnect" || errorState === "codeConsumed" || errorState === "disconnected"}
+			<button
+				class="btn btn-error"
+				on:click={async () => await startSessionCode()}
+			>Reconnect</button>
 		{/if}
 	</div>
 </div>
